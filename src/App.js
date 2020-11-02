16b65c8bf8419edbde2c0883f7420572374496cb
Yashwanth <yashwanth.1rn18cs423@gmail.com>
2020-11-02 13:54:54 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
+const App = () =>{
+  const APP_ID = "fbeb70de";
+  const APP_KEY = "c962492dfda10310e7da5becac437cb7";
+  
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  useEffect( () => {
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+    };
+    getRecipes();
+  },[query]);
+
+  
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
+        <button  type="submit" className="search-button">Search</button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe =>(
+          <Recipe
+            key={recipe.recipe.image}
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+      
     </div>
   );
 }
-
 export default App;
